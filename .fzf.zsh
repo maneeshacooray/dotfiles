a80430e31f565e900e2cf4b31fa090cd5f3e0684
Maneesha Cooray <bmaneeshacooray@gmail.com>
2026-07-14 21:01:09 +0530

    feat(fzf): update fzf dots

--- .fzf.zsh
+++ .fzf.zsh
@@ -4,4 +4,4 @@ if [[ ! "$PATH" == */opt/homebrew/opt/fzf/bin* ]]; then
   PATH="${PATH:+${PATH}:}/opt/homebrew/opt/fzf/bin"
 fi
 
-source <(fzf --zsh)
+source <(fzf --zsh 2>/dev/null | grep -v '^bindkey.*fzf-completion' || fzf --zsh)
